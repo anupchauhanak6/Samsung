e8fdb151f75c4085e347e75cd0578d4535c38113
Anup Chauhan <anupchauhanak6@gmail.com>
2025-09-24 00:06:39 +0530

    add shop page

---
+++ frontend/src/Pages/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Components/Header'
+
+function Shop() {
+  return (
+    <div className='bg-amber-200'>
+        
+    </div>
+  )
+}
+
+export default Shop
