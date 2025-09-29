aa6f6f6c0653343d69aade299c8d32d12f517c6b
Anup Chauhan <anupchauhanak6@gmail.com>
2025-09-29 11:11:14 +0530

    make img responsive

--- frontend/src/Components/HeroSection.jsx
+++ frontend/src/Components/HeroSection.jsx
@@ -5,9 +5,10 @@ function HeroSection() {
   return (
     <>
       <div className='w-full h-full relative'>
-          <img className=' ' src={HeroImg} alt='HeroImage'></img>
-      <div className="flex justify-center items-center z-10 absolute top-[60%] left-[5%] gap-[2vh]">
+          <img className='hidden sm:block' src={HeroImg} alt='HeroImage'></img>
+          <img className='block sm:hidden' src='https://images.samsung.com/is/image/samsung/assets/in/home/09-2025/HOME_TS11_Hero-KV_720x1248_mo.jpg?$720_N_JPG$?imwidth=720'></img>
 
+        <div className="flex justify-center items-center z-10 absolute top-[60%] left-[5%] gap-[2vh]">
           <button className="cursor-pointer relative text-white font-semibold after:content-[''] after:absolute after:w-0 after:h-[1px] after:bg-white after:left-0 after:bottom-[calc(var(--spacing)*-0.61)] after:transition-all after:duration-300 hover:after:w-full underline decoration-1 underline-offset-8 hover:no-underline">
             Learn more
           </button>
