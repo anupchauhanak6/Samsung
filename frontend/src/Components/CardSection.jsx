16b3f3873cd6866fb4e3c67c000c1a3402ddbbbf
Anup Chauhan <anupchauhanak6@gmail.com>
2025-09-29 11:08:57 +0530

    create CardSection

---
+++ frontend/src/Components/CardSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CardSection(props) {
+  return (
+    <div className='relative w-[25%]'>
+      <img src={props.img} className=''></img>
+    </div>
+  )
+}
+
+export default CardSection
