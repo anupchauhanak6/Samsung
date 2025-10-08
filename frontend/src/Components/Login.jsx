c24b901ecc367dd464a97ac3157bc4dd0d4d1d3d
Anup Chauhan <anupchauhanak6@gmail.com>
2025-10-08 23:30:41 +0530

    create login page

---
+++ frontend/src/Components/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Login() {
+
+  return (
+    <div className='m-auto max-w-[1440px] w-full pt-[12px]'>
+        <div className="p-[20px_24px] font-[700] leading-[31.92px] text-center">
+            <h3 className='block w-full text-[24px] p-[5px_0_20px]'>Login</h3>
+            <h4 className='block text-[18px] p-[0_0_22px]'>Login with mobile to become a member</h4>
+            <div></div>
+            <div></div>
+            <p>
+                {'By logging-in, I consent and accept the Smart club Program and Enrollment '}
+            </p>
+        </div>
+    </div>
+  );
+}
+
+export default Login;
