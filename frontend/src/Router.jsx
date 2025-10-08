27bebbcd73117bdea7502106ce7ca3e4b05ace51
Anup Chauhan <anupchauhanak6@gmail.com>
2025-10-08 23:30:26 +0530

    add login

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -3,13 +3,15 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './Pages/Home'
 import Shop from './Pages/Shop'
 import Mobile from './Pages/Mobile'
+import Login from './Components/Login'
 
 const AppRouter = () => {
   return (
     <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/shop' element={<Shop />}></Route>
-        <Route path='/mobile' element={<Mobile />}></Route>
+        <Route path='/' element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/mobile' element={<Mobile />} />
+        <Route path='/login' element={<Login />} />
     </Routes>
   )
 }
