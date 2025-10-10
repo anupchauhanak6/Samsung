e0fa835722a6fb6471b858897b2c0703a1981583
Anup Chauhan <anupchauhanak6@gmail.com>
2025-10-10 13:10:07 +0530

    upadte in MobileBar

--- frontend/src/Components/MobileBar.jsx
+++ frontend/src/Components/MobileBar.jsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react'
-import { Link,NavLink} from 'react-router-dom'
+import { Link,NavLink, useLocation} from 'react-router-dom'
 import { HiMiniBars3 } from "react-icons/hi2";
 import { RxCross2 } from "react-icons/rx";
 import { GoSearch } from "react-icons/go";
 
 function MobileBar() {
+    const location = useLocation()
+    const isLogin = location.pathname==='/login'
     const [menuOpen, setMenuOpen] = useState(false)
     return (
         <>
@@ -13,8 +15,9 @@ function MobileBar() {
                     <HiMiniBars3 className='h-[24px] w-[24px]'/>
                 </button>
             </div>
-            <div className={`fixed top-0 right-0 h-full w-full sm:max-w-[360px] bg-white z-10 transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out`}>
-                 {/* Close Button */}
+            <div  className={`fixed top-0 right-0 h-full w-full sm:max-w-[360px] bg-white z-10 transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out ${isLogin?'hidden':''}`}>
+
+                {/* Close Button */}
                 <div className="p-4">
                     <div className="flex items-center gap-2">
                         <button type='button' className={`flex items-center border border-[rgba(255,255,255,0.2)] p-[9px_12px_10px] gap-[8px] w-[87.804878%] rounded-[100px] bg-[#f7f7f7] text-black cursor-pointer relative justify-start`}>
@@ -29,6 +32,8 @@ function MobileBar() {
                         </button>
                     </div>
                 </div>        
+
+                {/* List data */}
                 <ul className="flex flex-col gap-4 p-4 text-black font-bold text-2xl">
                     <li><NavLink to='/shop' onClick={() => setMenuOpen(false)}>Shop</NavLink></li>
                     <li><NavLink to='/mobile' onClick={() => setMenuOpen(false)}>Mobile</NavLink></li>
@@ -38,6 +43,29 @@ function MobileBar() {
                     <li><NavLink to='/accessories' onClick={() => setMenuOpen(false)}>Accessories</NavLink></li>
                 </ul>
             </div>
+
+            {/* Login page NavBar */}
+            <div className={`fixed top-0 right-0 h-full w-full sm:max-w-[360px] bg-white z-10 transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out ${isLogin?'':'hidden'}`}>
+
+                {/* Close Button */}
+                <div className="p-4">
+                    <div className="flex items-center gap-2">
+                        <button type='button' className={`flex items-center border border-[rgba(255,255,255,0.2)] p-[9px_12px_10px] gap-[8px] w-[87.804878%] rounded-[100px] bg-[#f7f7f7] text-black cursor-pointer relative justify-start`}>
+                            <span className='order-2 absolute!important '>Search</span>
+                            <GoSearch className='w-[16px] h-[16px] '/>
+                        </button>
+                        <button
+                            onClick={() => setMenuOpen(false)}
+                            className="text-2xl text-black"
+                        >
+                            <RxCross2 className='h-[24px] w-[24px]'/>
+                        </button>
+                    </div>
+                </div>
+                {/* List data */}
+                <ul></ul>
+            </div>
+
              {/* Background Overlay */}
             {menuOpen && (
                 <div
