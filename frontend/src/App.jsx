2ef494e0260cd2e011c00ec3d44fa96ad7d3b890
Anup Chauhan <anupchauhanak6@gmail.com>
2025-10-11 18:19:13 +0530

    update with userProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,19 @@ import Footer from './Components/Footer'
 import AppRouter from './Router'
 import LoginHeader from './Components/LoginHeader'
 import { useLocation } from 'react-router-dom'
+import UserProvider from './contexts/UserProvider'
 
 function App() {
   const location = useLocation()
   const isLoginPage = location.pathname === '/login';
   return (
-    <div className='overflow-x-hidden flex flex-col items-center'>
-      {isLoginPage ? <LoginHeader /> : <Header />}
-      <AppRouter />
-      <Footer />
-    </div>
+    <UserProvider>
+      <div className='overflow-x-hidden flex flex-col items-center max-w-[1440px] justify-center m-auto'>
+        {isLoginPage ? <LoginHeader /> : <Header />}
+        <AppRouter />
+        <Footer />
+      </div>
+    </UserProvider>
   )
 }
 
