3e6bd988e553d08031e2ebff5fb98891e7721caa
Anup Chauhan <anupchauhanak6@gmail.com>
2025-10-11 18:25:58 +0530

    update login page

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,42 +1,91 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import UserContext from "../contexts/UserContext";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState("");
+    const { setUser } = useContext(UserContext);
+
+    const validateInput = (val) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const phoneRegex = /^[0-9]{10}$/;
+        return emailRegex.test(val) || phoneRegex.test(val);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (email.trim() === "") {
+        setError("Please login using your Email ID/Mobile Number.");
+        return; // Submit rok do
+        }
+        if (!validateInput(email.trim())) {
+            setError("Please login using your Email ID/Mobile Number.");
+            return;
+        }
+        setError("");
+        return setUser(email);
+    };
 
   return (
-    <div className='m-auto max-w-[1440px] w-full pt-[12px]'>
-        <div className="p-[20px_24px] leading-[31.92px] text-center">
-            <h3 className='block w-full text-[24px] p-[5px_0_20px] font-[700]'>Login</h3>
-            <h4 className='block text-[18px] p-[0_0_22px] font-[700]'>Login with mobile to become a member</h4>
-            <div className='p-[15px_0_0] w-[33vw] h-[80px] m-auto table relative'>
-                <div className='w-full m-0 relative'>
-                    <label htmlFor='username' id='username' className='absolute top-[3px] left-0 text-start p-[10px_0_5px] text-[14px] text-[#766767] font-[500] w-auto  pointer-events-none'>Mobile Number / Email Id</label>
-                    <input type='text' id='username' className='border-b border-[#8f8f8f] focus:outline-0 text-[14px] font-[400] w-full p-[10px_0_5px]'></input>
-                    <div className='text-[#dc2222] text-[12px] leading-[14px] mt-[5px] p-[5px_0]'></div>
-                </div>
+    <>
+        <div className="m-auto max-w-[1440px] w-full pt-[12px]">
+            <div className="p-[20px_24px] leading-[31.92px] text-center">
+                <h3 className="block w-full text-[24px] p-[5px_0_20px] font-[700]">
+                    Login
+                </h3>
+                <h4 className="block text-[18px] p-[0_0_22px] font-[700]">
+                    Login with mobile to become a member
+                </h4>
+                <div className="p-[15px_0_0] w-[33vw] h-[80px] m-auto table relative">
+                    <form onSubmit={handleSubmit}>
+                        <div className="w-full m-0 relative">
+                            <label htmlFor="username" id="username" className="absolute top-[3px] left-0 text-start p-[10px_0_5px] text-[14px] text-[#766767] font-[500] w-auto  pointer-events-none">Mobile Number / Email Id</label>
 
-                <div className='leading-[18.62px] justify-start flex'>
-                    <input type='checkbox' className={`appearance-none outline-none cursor-pointer w-[18px] h-[18px] bg-white border rounded-full checked:bg-blue-500 checked:bg-[url(https://upload.wikimedia.org/wikipedia/commons/2/27/White_check.svg)] checked:bg-center checked:bg-no-repeat checked:bg-[length:14px_14px] transition`}></input>
-                    <span className='w-full font-normal text-start'>
-                        {'I agree to receive personalized offers and updates'}
-                        <br/>
-                        {'You can opt out anytime by visiting My Page'}
-                    </span>
-                </div>
+                            <input onChange={(e) => setEmail(e.target.value)} type="text" id="username"className="border-b border-[#8f8f8f] focus:outline-0 text-[14px] font-[400] w-full p-[10px_0_5px]"/>
 
-                <div>
-                    <button></button>
+                            <div className="text-[#dc2222] text-start text-[12px] leading-[14px] mt-[5px] p-[5px_0]">{error}</div>
+                        </div>
+
+                        <div className="leading-[18.62px] justify-start p-[0_0_20px_30px]">
+                            <input type="checkbox" className={`float-left -ml-[30px] appearance-none outline-none cursor-pointer w-[20px] h-[20px] bg-white border rounded-full checked:bg-blue-500 checked:bg-[url(https://www.samsung.com/in/web/dist/img/icon-checked-white.f836399.svg)] checked:bg-center checked:bg-no-repeat checked:border-blue-500 checked:bg-[length:14px_14px] transition`}></input>
+                            
+                            <span className="w-full inline-block font-normal text-[14px] leading-[18.62px] text-start">
+                                {"I agree to receive personalized offers and updates"}
+                                <br />
+                                {"You can opt out anytime by visiting My Page"}
+                            </span>
+                        </div>
+
+                        <p className="p-[25px_0] text-center font-[700] text-[1.11111vw] leading-[200%]">
+                            <button type="submit" className="cursor-pointer h-[40px] w-[296px] border bg-black text-white rounded-[20px]">Login</button>
+                        </p>
+
+                        <p className="pb-[25px] text-center font-[700] text-[1.11111vw] leading-[200%]">
+                            <button className="cursor-pointer h-[40px] w-[296px] border bg-[#2189ff] text-white rounded-[20px]">Login via Samsung Account</button>
+                        </p>
+                    </form>
                 </div>
+                <div></div>
+                <p className="font-[400] text-[14px]">
+                    {
+                    "By logging-in, I consent and accept the Smart club Program and Enrollment "
+                    }
+                    <a
+                    href="https://images.samsung.com/is/content/samsung/assets/in/microsite/smart-club/smartclub-terms-conditions.pdf?v=4"
+                    target="_blank"
+                    className="cursor-pointer text-[#20a2ff]"
+                    >
+                    {"Terms and Conditions "}
+                    </a>
+                    {"& "}
+                    <Link to={""} className="cursor-pointer text-[#20a2ff]">
+                        {"Privacy Policy"}
+                    </Link>
+                </p>
             </div>
-            <div></div>
-            <p className='font-[400] text-[14px]'>
-                {'By logging-in, I consent and accept the Smart club Program and Enrollment '}
-                <a href='https://images.samsung.com/is/content/samsung/assets/in/microsite/smart-club/smartclub-terms-conditions.pdf?v=4' target='_blank' className='cursor-pointer text-[#20a2ff]'>{'Terms and Conditions '}</a>
-                {'& '}
-                <Link to={''} className='cursor-pointer text-[#20a2ff]'>{'Privacy Policy'}</Link>
-            </p>
         </div>
-    </div>
+    </>
   );
 }
 
