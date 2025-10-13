043d3afe8c63f3b2db733301f3cbdb493c68ab41
Anup Chauhan <anupchauhanak6@gmail.com>
2025-10-13 22:55:28 +0530

    change hore img

--- frontend/src/Components/HeroSection.jsx
+++ frontend/src/Components/HeroSection.jsx
@@ -8,7 +8,7 @@ function HeroSection() {
     <>
       <div className='max-w-[1440px] h-full flex relative m-auto'>
           <img className='hidden sm:block h-fit w-full' src='https://images.samsung.com/is/image/samsung/assets/in/home/10-2025/25_bf_hero-kv_preview_pc_1920x1080_text_.jpg?$1920_N_JPG$?imwidth=2560' alt='HeroImage'></img>
-          <img className='block sm:hidden' src='https://images.samsung.com/is/image/samsung/assets/in/home/09-2025/HOME_TS11_Hero-KV_720x1248_mo.jpg?$720_N_JPG$?imwidth=720'></img>
+          <img className='block sm:hidden' src='https://images.samsung.com/is/image/samsung/assets/in/home/10-2025/25_bf-hero_kv_mo_720x1120.jpg?$720_N_JPG$?imwidth=720'></img>
 
         <div className="absolute left-[50%] top-[50%] -translate-y-[50%] w-[43.05555556vw] pt-[3.61111111vw] pb-[4.16666667vw] pl-[4.16666667vw] -ml-[50vw] mt-[52.7031px] text-[#fff]">
           <h1 className='text-[4.16666667vw] font-bold leading-[1.2]'>
