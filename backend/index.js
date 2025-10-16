ec51580fa7f52f6ab737699316bdcc670276584c
Anup Chauhan <anupchauhanak6@gmail.com>
2025-10-16 23:35:55 +0530

    make backend and setup

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('server is running...')
+})
+
+const PORT = 3000
+
+app.listen(PORT,()=>{
+    console.log('Port 3000');
+    
+})
