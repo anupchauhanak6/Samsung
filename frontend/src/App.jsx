5e399db99de9e2746f02a5927e9f4f1fda709b02
Anup Chauhan <anupchauhanak6@gmail.com>
2025-10-29 22:00:46 +0530

    update App.jsx and add Layout file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,15 @@
 import React from 'react'
 import './App.css'
-import Home from './Pages/Home'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
 import AppRouter from './Router'
-import LoginHeader from './Components/LoginHeader'
-import { useLocation } from 'react-router-dom'
 import UserProvider from './contexts/UserProvider'
+import Layout from './Components/Layout'
 
 function App() {
-  const location = useLocation()
-  const isLoginPage = location.pathname === '/login';
-  const isCartPage = location.pathname === '/cart';
-  const isSignupPage = location.pathname === '/signup';
   return (
     <UserProvider>
-      <div className='overflow-x-hidden flex flex-col items-center max-w-[1440px] justify-center m-auto'>
-        {isLoginPage || isCartPage || isSignupPage ? <LoginHeader /> : <Header />}
+      <Layout>
         <AppRouter />
-        <Footer />
-      </div>
+      </Layout>
     </UserProvider>
   )
 }
