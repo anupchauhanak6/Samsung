6e90b8019b73671a2e125abfe98f592d40883a63
Anup Chauhan <anupchauhanak6@gmail.com>
2025-10-30 11:07:28 +0530

    connect express, cors, route and also connect mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,24 @@
-const express = require('express')
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import { connectDb } from './config/db.js'
+import userRouter from './routes/route.js'
 
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.send('server is running...')
-})
-app.get('/cart',(req,res)=>{
-    res.send('This is cart')
+app.use(express.json())
+
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}))
+
+app.use('/api',userRouter)
+
+connectDb().
+then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log('This is Port' , process.env.PORT );
+    })
 })
 
-const PORT = 3000
-
-app.listen(PORT,()=>{
-    console.log('This is Port 3000');
-})
