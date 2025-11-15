6a26ef02bed4d7d90e8a6da37a7b482d8a3c90e0
Anup Chauhan <anupchauhanak6@gmail.com>
2025-11-15 16:28:32 +0530

    refactor: reorganize server initialization and add cookie parser

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,47 @@
-import express from 'express'
-import 'dotenv/config'
-import cors from 'cors'
-import { connectDb } from './config/db.js'
-import userRouter from './routes/route.js'
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import { connectDb } from "./config/db.js";
+import userRouter from "./routes/route.js";
+import cookieParser from "cookie-parser";
+import { connectCloudinary } from "./config/cloudinary.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cors({
+app.use(cookieParser());
+
+app.use(
+  cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL
-}))
+    origin: process.env.FRONTEND_URL,
+  })
+);
 
-app.use('/api',userRouter)
+app.get("/", (req, res) => {
+  res.send("Samsung Backend is running");
+});
 
-connectDb().
-then(()=>{
-    app.listen(process.env.PORT,()=>{
-        console.log('This is Port' , process.env.PORT );
-    })
-})
+app.use("/api", userRouter);
 
+connectDb().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log("This is Port", process.env.PORT);
+  });
+});
+
+// Start server only DB & cloudinary connect
+
+export const startServer = async () => {
+  try {
+    await connectDb();
+    await connectCloudinary();
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
